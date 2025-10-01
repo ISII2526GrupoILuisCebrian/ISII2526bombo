08eb9ffe0a66a3df4f69dca55d7c9bd66762d7ed
Santis Mednieks <Santis.Mednieks1@alu.uclm.es>
2025-10-01 16:24:13 +0200

    Added a new class "PurchaseDelivery" along with attributes.

---
+++ src/AppForSEII2526.API/Models/PurchaseDelivery.cs
@@ -0,0 +1,26 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(PurchaseOrderInt), IsUnique = true)]
+    public class PurchaseDelivery
+    {
+        [Key]
+        public int DeliveryAssignmentId { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        [Required]
+        public int PurchaseOrderInt { get; set; }
+
+        public PurchaseDelivery()
+        {
+
+        }
+
+        enum PriorityType
+        {
+            Low,
+            Medium,
+            High
+        }
+    }
+}
