ac08c85b1fc7e27865785b4e354e82db5a35f2b2
luisCebsoo <luis.cebrian1@alu.uclm.es>
2025-10-02 10:45:53 +0200

    atributes defined(missing relations)

---
+++ src/AppForSEII2526.API/Models/ReportCustomer.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(BanReportId),
+    nameof(CustomerId))]
+    public class ReportCustomer
+    {
+        
+        public int BanReportId { get; set; }        
+        public int CustomerId { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
