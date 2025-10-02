fec6b6e35ad743bbda60c607d5a15b3913770069
luisCebsoo <luis.cebrian1@alu.uclm.es>
2025-10-03 00:31:20 +0200

    Creation of ReportCustomer class and its atributes defined(missing relations) (#17)
    * atributes defined(missing relations)
    
    * I created the ReportCustomer class and its attributes
    
    * testing purpose
    
    * testing purpose 2
    
    * added attribute to ReportCustomer for testing. It doesn't let me do a pull request, it does not appear in github
    
    * just checked the issue, proceed

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,5 +6,7 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Product> Products { get; set; }
+
+    public DbSet<ReportCustomer> ReportCustomers { get; set; }
     public DbSet<BanReport> BanReports { get; set; }
 }
