921895e94071074e975252a415eace375e2edf18
luisCebsoo <luis.cebrian1@alu.uclm.es>
2025-10-03 01:15:40 +0200

    I implemented ApplicationUser class in its corresponding branch

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -2,6 +2,24 @@
 
 namespace AppForSEII2526.API.Models;
 
+
+[Index(nameof(Name), nameof(Surname), nameof(Address), nameof(AccountCreationDate), IsUnique = true)]
 // Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
+public class ApplicationUser : IdentityUser
+{
+
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Account creation date")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime AccountCreationDate { get; set; }
+
+    [StringLength(200, ErrorMessage = "Address can be neither longer than 200 characters nor shorter than 10.", MinimumLength = 10)]
+    public string Address { get; set; }
+
+    [StringLength(50, ErrorMessage = "Name can be neither longer than 50 characters nor shorter than 10.", MinimumLength = 10)]
+    [Key]
+    public string Name { get; set; }
+
+    [StringLength(50, ErrorMessage = "Surname can be neither longer than 50 characters nor shorter than 10.", MinimumLength = 10)]
+    public string Surname { get; set; }
+
 }
