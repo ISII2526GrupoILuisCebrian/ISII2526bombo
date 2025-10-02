a0da12f11650e2f8b04c3067c367a0333ef632b7
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-03 00:45:38 +0200

    ApplicationUser recuperada

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,9 +5,9 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-<<<<<<< HEAD
+
     public DbSet<PurchaseProduct> PurchaseProducts { get; set; }
-=======
+
     public DbSet<Product> Products { get; set; }
->>>>>>> origin/development
+
 }
