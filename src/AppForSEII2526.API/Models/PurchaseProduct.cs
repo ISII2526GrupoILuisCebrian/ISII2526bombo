c004b78308d0b4d658670b6fcd2d49e5a549f0b8
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-02 23:58:16 +0200

    Added restrictions for quantity and price

--- src/AppForSEII2526.API/Models/PurchaseProduct.cs
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -3,9 +3,14 @@
     public class PurchaseProduct
     {
         public int Id { get; set; }
+
+        [Precision(7, 2)]
         public decimal Price { get; set; }
+
         public int ProductId { get; set; }
         public int PurchaseOrderId { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Quantity must be between 1 and 10")]
         public int Quantity { get; set; }
     }
 }
