24cdefd3f23796a4a29f788b4590f8bec0e63b90
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-02 23:49:38 +0200

    PurchaseProduct class created

---
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseProduct
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int ProductId { get; set; }
+        public int PurchaseOrderId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
