93a5542dafdf96529d74dee5826b00368d992346
Santis Mednieks <Santis.Mednieks1@alu.uclm.es>
2025-10-03 07:00:02 +0200

    This is a middle base for 2 future classes, requires different configuration for relationships (done after adding purchase order and delivery).... made the changes, should be good now

--- src/AppForSEII2526.API/Models/PurchaseDelivery.cs
+++ src/AppForSEII2526.API/Models/PurchaseDelivery.cs
@@ -1,14 +1,9 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-    [Index(nameof(PurchaseOrderInt), IsUnique = true)]
     public class PurchaseDelivery
     {
-        [Key]
         public int DeliveryAssignmentId { get; set; }
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
-        [Required]
         public int PurchaseOrderInt { get; set; }
 
 
