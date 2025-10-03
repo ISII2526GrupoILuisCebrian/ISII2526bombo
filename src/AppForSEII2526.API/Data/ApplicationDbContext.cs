52b10f1b0a0cad2b0e347d9aabea6f80baec2255
angelpieiga10 <angel.pieiga@alu.uclm.es>
2025-10-03 02:01:36 +0200

    Task4 add purchase product class to model folder (#22)
    * PurchaseProduct class created
    
    * Added restrictions for quantity and price
    
    * ApplicationUser recuperada
    
    * ApplicationDbContext problems are solved
    
    ---------
    
    Co-authored-by: luisCebsoo <luis.cebrian1@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,9 +5,13 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<PurchaseProduct> PurchaseProducts { get; set; }
+
     public DbSet<Product> Products { get; set; }
 
     public DbSet<ReportCustomer> ReportCustomers { get; set; }
+
     public DbSet<BanReport> BanReports { get; set; }
 
     public DbSet<ComplaintType> ComplaintTypes { get; set; }
