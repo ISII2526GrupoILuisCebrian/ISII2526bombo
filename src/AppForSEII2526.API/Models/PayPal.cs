f1408ca3fda4a7403d838b911803e721524fcc1d
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-03 04:07:18 +0200

    "PayPal" class is created

---
+++ src/AppForSEII2526.API/Models/PayPal.cs
@@ -0,0 +1,8 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PayPal : PaymentMethod
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+    }
+}
