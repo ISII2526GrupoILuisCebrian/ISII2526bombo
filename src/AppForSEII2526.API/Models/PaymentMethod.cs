7549dcf942c1dbec819e8d29f724e239bc3441f2
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-03 20:25:29 +0200

    Relation 1 to 1 eliminated from "PaymetnMethod"

--- src/AppForSEII2526.API/Models/PaymentMethod.cs
+++ src/AppForSEII2526.API/Models/PaymentMethod.cs
@@ -3,8 +3,5 @@
     public abstract class PaymentMethod
     {
         public int Id { get; set; }
-
-        //Relations
-        public PurchaseOrder PurchaseOrder { get; set; } // 1 to 1 with PurchaseOrder
     }
 }
