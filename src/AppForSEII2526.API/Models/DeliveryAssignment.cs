10a719ed7d24c9f301aabacc7c6d5324308a6cf0
Santis Mednieks <Santis.Mednieks1@alu.uclm.es>
2025-10-03 09:37:34 +0200

    adding the required enum class

---
+++ src/AppForSEII2526.API/Models/DeliveryAssignment.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+
+namespace AppForSEII2526.API.Models
+{
+    public enum PriorityType
+    {
+        High,
+        Medium,
+        Low
+    }
+}
