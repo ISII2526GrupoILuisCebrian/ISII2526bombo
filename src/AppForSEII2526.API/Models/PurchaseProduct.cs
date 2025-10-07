4040ed37f88d12fcef107addbb9baa025a164bf3
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-07 22:45:46 +0200

    Modificationg the class "PurchaseProduct" establishing the correct primary keys and foreign keys

--- src/AppForSEII2526.API/Models/PurchaseProduct.cs
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -1,21 +1,27 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(ProductId), nameof(PurchaseOrderId))]
     public class PurchaseProduct
     {
-        public int Id { get; set; }
-
+        
         [Precision(7, 2)]
         public decimal Price { get; set; }
 
         public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
+
         public int PurchaseOrderId { get; set; }
+
+        [ForeignKey(nameof(PurchaseOrderId))]
+        public PurchaseOrder PurchaseOrder { get; set; }
 
         [Range(1, 10, ErrorMessage = "Quantity must be between 1 and 10")]
         public int Quantity { get; set; }
 
-        public Product Product { get; set; }
+       
 
-        public PurchaseOrder PurchaseOrder { get; set; }
-
+        
     }
 }
