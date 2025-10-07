8224a2dfb538953b0eac5b0ccf54cc4bdaf83f54
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-07 22:51:39 +0200

    Change in class "Bizum" set the TelephoneNumber attribute to be a string ans certain requirements

--- src/AppForSEII2526.API/Models/Bizum.cs
+++ src/AppForSEII2526.API/Models/Bizum.cs
@@ -2,6 +2,9 @@
 {
     public class Bizum : PaymentMethod
     {
+        [Required]
+        [RegularExpression(@"^\+?[0-9]+$", ErrorMessage = "El número de teléfono solo puede contener dígitos y un '+' opcional al inicio.")]
+        [StringLength(15, MinimumLength = 7, ErrorMessage = "El número de teléfono debe tener entre 7 y 15 dígitos.")]
         [Display(Name = "Telephone Number")]
         public string TelephoneNumber { get; set; }
     }
