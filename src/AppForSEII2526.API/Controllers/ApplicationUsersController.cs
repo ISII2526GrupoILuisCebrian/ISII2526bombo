da37487be508ce279e1fb437a6ec8f4aaa817d57
luisCebsoo <luis.cebrian1@alu.uclm.es>
2025-10-13 17:13:36 +0200

    Creating the ApplicationUsers Controller

---
+++ src/AppForSEII2526.API/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUsersController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ApplicationUsersController> _logger;
+
+        public ApplicationUsersController(ApplicationDbContext context, ILogger<ApplicationUsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal a, decimal b)
+        {
+            if(b == 0)
+            {
+                string error = "B cannot be 0 to compute a division";
+                _logger.LogError(DateTime.Now + " Error:" + error);
+                return BadRequest(error);
+            }
+            decimal result = a / b;
+            return Ok(result);
+        }
+        */
+
+
+    }
+}
