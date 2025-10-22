0d7f195d7fdae643888336abf54b508f5bbe3515
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-22 10:49:40 +0200

    Data Uploaded, controller exception

--- src/AppForSEII2526.API/Controllers/ProductsController.cs
+++ src/AppForSEII2526.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using AppForSEII2526.API.DTOs.ProductDTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 
@@ -39,16 +40,52 @@ namespace AppForSEII2526.API.Controllers
         [HttpGet]
         [Route("[action]")]
         [ProducesResponseType(typeof(IList<ProductForPurchasingDTO>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult> GetProductsForPurchasing(string? productName)
+        [ProducesResponseType(typeof(ModelError), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> GetProductsForPurchasing(string? productName, string? colour)
         {
+            if (string.IsNullOrWhiteSpace(productName) && string.IsNullOrWhiteSpace(colour))
+            {
+                _logger.LogWarning($"{DateTime.Now} Warning: No filters were provided for product search.");
+            }
+
+            var productsQuery = _context.Products
+                .Include(p => p.Brand) // incluir marca para poder sacar su nombre
+                .Include(p => p.PurchaseProducts) // incluir compras
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(productName))
+            {
+                productsQuery = productsQuery.Where(p => p.Name.Contains(productName));
+            }
+
+            if (!string.IsNullOrWhiteSpace(colour))
+            {
+                productsQuery = productsQuery.Where(p => p.Colour.Contains(colour));
+            }
+
             IList<ProductForPurchasingDTO> productsDTOS = await _context.Products
                 .Include(product=>product.Brand)
-                .Where(product=>product.Name.Contains(productName)
-                    || (productName == null))
+                .Include(product=>product.PurchaseProducts)
+                .Where(product=>
+                 (productName == null || product.Name.Contains(productName)) &&
+                 (colour == null || product.Colour.Contains(colour)) &&
+                 product.Stock > 0)
                 .OrderBy(product=>product.Name)
-                .Select(product => new ProductForPurchasingDTO(product.Id, product.Name, product.Brand.Name))
+                 .Select(product => new ProductForPurchasingDTO(
+                    product.Id,
+                    product.Name,
+                    product.Brand.Name,
+                    product.PurchaseProducts.Sum(pp => pp.Quantity), // total comprado
+                    product.Brand.Location
+                  ))
                 .ToListAsync();
+            if (!productsDTOS.Any())
+            {
+                return BadRequest("There are no products available to purchase.");
+            }
+
             return Ok(productsDTOS);
+
         }
     }
 }
