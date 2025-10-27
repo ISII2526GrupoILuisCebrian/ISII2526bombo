bac63089422d521c628042adcc2d06f23cc903c0
ÁNGEL PIEIGA GONZÁLEZ <Angel.Pieiga@alu.uclm.es>
2025-10-27 17:12:24 +0100

    PurchaseForDetail completed

--- src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseForDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseForDetailDTO.cs
@@ -1,6 +1,37 @@
-﻿namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
 {
-    public class PurchaseForDetailDTO
+    public class PurchaseForDetailDTO : PurchaseForCreateDTO
     {
+        public PurchaseForDetailDTO(
+            int id,
+            DateTime purchaseDate,
+            string street,
+            string postalCode,
+            PurchaseState state,
+            decimal totalPrice,
+            string nameCustomer,
+            string surnameCustomer,
+            PaymentMethod paymentMethod,
+            string userNameCustomer,
+            IList<PurchaseProductDTO> purchaseProducts)
+
+            : base(street, postalCode, nameCustomer, surnameCustomer, purchaseDate, paymentMethod, userNameCustomer, purchaseProducts)
+        {
+            Id = id;
+            TotalPrice = totalPrice;
+
+        }
+
+        public int Id { get; set; }
+
+        [Precision(10, 2)]
+        public decimal TotalPrice { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+
+
     }
 }
