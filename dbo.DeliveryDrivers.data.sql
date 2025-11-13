fc4024d14f4baa5c05b7f2ef294d815146fc04c1
Santis Mednieks <Santis.Mednieks1@alu.uclm.es>
2025-11-13 21:23:02 +0100

    added scripts for sql

---
+++ dbo.DeliveryDrivers.data.sql
@@ -0,0 +1,15 @@
+﻿SET IDENTITY_INSERT [dbo].[DeliveryDrivers] ON;
+
+INSERT INTO [dbo].[DeliveryDrivers] ([Id], [Available], [Name]) 
+VALUES 
+    (1, 'TRUE', 'Marco Rossi');
+
+INSERT INTO [dbo].[DeliveryDrivers] ([Id], [Available], [Name]) 
+VALUES 
+    (2, 'TRUE', 'Sofia Bianchi');
+
+INSERT INTO [dbo].[DeliveryDrivers] ([Id], [Available], [Name]) 
+VALUES 
+    (3, 'FALSE', 'Luca Verdi');
+
+SET IDENTITY_INSERT [dbo].[DeliveryDrivers] OFF;
