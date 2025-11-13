c88198135f67d893a33c10e8c6cef94f4832dafa
Santis Mednieks <Santis.Mednieks1@alu.uclm.es>
2025-11-13 23:21:18 +0100

    created the second Get method

---
+++ src/AppForSEII2526.API/DTOs/DeliveryDTOs/SchedulingDetailsDTO.cs
@@ -0,0 +1,17 @@
+﻿using AppForSEII2526.API.DTOs.DeliveryDriverDTOs;
+
+namespace AppForSEII2526.API.DTOs.DeliveryDTOs
+{
+    public class SchedulingDetailsDTO
+    {
+        public SchedulingDetailsDTO(IList<OrderForSchedulingDTO> selectedOrders, IList<DriverForAssignmentDTO> availableDrivers)
+        {
+            SelectedOrders = selectedOrders;
+            AvailableDrivers = availableDrivers;
+        }
+
+        public IList<OrderForSchedulingDTO> SelectedOrders { get; set; }
+
+        public IList<DriverForAssignmentDTO> AvailableDrivers { get; set; }
+    }
+}
